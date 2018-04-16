938ff67373408d551f6368ea40a53ac021324844
Дмитрий <sa-well-ev@yandex.ru>
2018-04-16 15:50:55 +0700

    Первый коммит
    Signed-off-by: Дмитрий <sa-well-ev@yandex.ru>

---
+++ config/web.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Дмитрий
+ * Date: 16.04.2018
+ * Time: 10:44
+ */
+return [
+  'id' => 'crmapp',
+  //'basePath' => realpath(__DIR__.'/../'),
+    'basePath' => dirname(__DIR__),
+    'components' => [
+      'request' => [
+        'cookieValidationKey' => '34jfjff44jffr75hrhdsfdsf',
+      ],
+    ],
+];
---
+++ controllers/SiteController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Дмитрий
+ * Date: 16.04.2018
+ * Time: 10:54
+ */
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class SiteController extends Controller
+{
+    public function actionIndex()
+    {
+        return 'Our CRM';
+    }
+}
